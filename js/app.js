4068fd5b3029493c3c76e250377198516666ab02
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-08-20 18:09:22 +0100

    committing wireframe and pseudocode

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("hello")
+// Footballer Top Trumps Pseudocode
+
+
+// ----MVP Pseudocode----
+// Must display two piles of cards on screen, one face-up (human), the other face-down (CPU)
+// Must select randomly who acts first (player or CPU) at start of game
+// Must display an indicator to show whose turn it is to act
+// Attributes shown on players cards must be clickable when it is player's turn to act
+// Must allow player to select an attribute to compare
+// Must compare the value associated with selected attribute with matching one from CPU card
+// Must evaluate who wins that round (trick) based on the higher value of the selected attribute
+// Must take card from loser and add to the bottom of winner's pile each trick
+// Must also move winner's card to bottom of their own deck after each trick
+// Must choose who acts next based on result of previous turn - if current player wins a trick, they select the attribute again in the next trick
+// Must 
+
+// ----Potential Additional Pseudocode----
+
+// Include "Three card pick" logic, which allows a player to select an attribute from their last 3 remaining cards
