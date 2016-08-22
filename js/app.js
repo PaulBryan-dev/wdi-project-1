decb5b6bb60abf2350c46faa8af6817eddcce53f
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-08-22 08:04:47 +0100

    layout adjusted

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 // ----MVP Pseudocode----
-// Must display two piles of cards on screen, one face-up (human), the other face-down (CPU)
+// Write a function to 
 // Must select randomly who acts first (player or CPU) at start of game
 // Must display an indicator to show whose turn it is to act
 // Attributes shown on players cards must be clickable when it is player's turn to act
@@ -17,3 +17,42 @@
 // ----Potential Additional Pseudocode----
 
 // Include "Three card pick" logic, which allows a player to select an attribute from their last 3 remaining cards
+
+
+
+$(document).ready(function(){
+
+
+  
+  
+
+  var $fooballers = $[
+      {name: "Lionel Messi",       shooting: 94, passing: 91, speed: 89, stamina: 82, dribbling: 96, tackling: 63, weakerFoot: 8, hair: 4, douchebaggery: 1},
+      {name: "Diego Costa",        shooting: 78, passing: 76, speed: 67, stamina: 73, dribbling: 56, tackling: 45, weakerFoot: 6, hair: 4, douchebaggery: 8},
+      {name: "Stephane Sessegnon", shooting: 79, passing: 76, speed: 81, stamina: 79, dribbling: 75, tackling: 41, weakerFoot: 7, hair: 6, douchebaggery: 5},
+      {name: "Bacary Sagna",       shooting: 62, passing: 67, speed: 73, stamina: 82, dribbling: 60, tackling: 78, weakerFoot: 6, hair: 1, douchebaggery: 2},
+      {name: "Joey Barton",        shooting: 67, passing: 74, speed: 69, stamina: 73, dribbling: 96, tackling: 79, weakerFoot: 3, hair: 5, douchebaggery: 9},
+      {name: "Mesut Ozil",         shooting: 76, passing: 88, speed: 74, stamina: 72, dribbling: 82, tackling: 44, weakerFoot: 7, hair: 7, douchebaggery: 3},
+      ];
+
+
+var playerCardCount = $(3);
+var cpuCardCount = $(3);
+$trickWinner = $(function() {
+  if (cpuCardCount === 0 && playercardCount === 6) {
+    alert("You Win!");
+  } else if (playercardCount === 0 && cpuCardCount === 50) {
+    alert("Yoo Lose!");
+  }
+});
+
+console.log(trickWinner)
+
+
+
+
+
+
+
+
+})
