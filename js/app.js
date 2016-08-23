6fb4c6b0553c9af495376f83b4c1f6691e08ae13
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-08-23 17:28:32 +0100

    functional MVP created (at last)

--- js/app.js
+++ js/app.js
@@ -1,120 +1,116 @@
-  // Footballer Top Trumps Pseudocode
+   // Footballer Top Trumps Pseudocode
 
 
-// ----MVP Pseudocode----
-// Write a function to compare the selected player attribute value with the corresponding CPU one, and return the higher value  ----IF/ELSE STATEMENT----
-// write a function to append both card objects from the current trick to the end of the winner's card object array
-// Write a function to select an attribute and pass its value into the compare function
-// Write a function so the CPU automatically selects the highest attribute vale
+  // ----MVP Pseudocode----
+  // Write a function to compare the selected player attribute value with the corresponding CPU one, and return the higher value  ----IF/ELSE STATEMENT----
+  // Write a function to select an attribute and pass its value into the compare function
+  // Write a function so the CPU automatically selects the highest attribute vale
+  // Write a function to append both card objects from the current trick to the end of the winner's card object array
 
-// ----Potential Additional Pseudocode----
+  // ----Potential Additional Pseudocode----
 
-// Include "Three card pick" logic, which allows a player to select an attribute from their last 3 remaining cards
+  // Include "Three card pick" logic, which allows a player to select an attribute from their last 3 remaining cards
 
-// window.onload()
+  // window.onload()
 
-$(start)
+  $(start)
 
 
-var playerCard;
-var computerCard;
+  var playerCard;
+  var computerCard;
 
-function start() {
-  pickCards();
+  function start() {
+    pickCards();
+  }
 
-  // console.log("Player Card", playerCard);
-  // console.log("Computer Card", computerCard);
-}
+  function pickCards() {
+    playerCard = footballers[Math.floor(Math.random() * footballers.length)];
+    computerCard = footballers[Math.floor(Math.random() * footballers.length)];
+    appendCards()
+  }
 
-function pickCards() {
-  playerCard = footballers[Math.floor(Math.random() * footballers.length)];
-  computerCard = footballers[Math.floor(Math.random() * footballers.length)];
+  function appendCards() {
+    appendPlayer(playerCard, "player");
+    appendPlayer(computerCard, "computer");
+    $('.player').addClass("flipped");
 
-  appendCards()
-}
+    selectAttr();
+  }
 
-function appendCards() {
-  $('#gameContainer').append(
-      "<div id='playerCard'>" +
-        "<h5>" + playerCard.name + "</h5>" +
-        "<img src=" + playerCard.img + ">" +
-        "<ul>" +
-          "<li>Shooting " + playerCard.shooting + "</li>" +
-          "<li>Passing " + playerCard.passing + "</li>" +
-          "<li>Speed " + playerCard.speed + "</li>" +
-          "<li>Stamina " + playerCard.stamina + "</li>" +
-          "<li>Dribbling " + playerCard.dribbling + "</li>" +
-          "<li>Tackling " + playerCard.tackling + "</li>" +
-          "<li>Weak Foot " + playerCard.weakerFoot + "</li>" +
-          "<li>Douchery " + playerCard.douchery + "</li>" +
-      "</div>"
+  function appendPlayer(player, id) {
+  $('.'+id+' figure.back').append(
+        "<h5>" + player.name + "</h5>" +
+        "<img src=" + player.img + ">" +
+        "<ul class="+ id +">" +
+        "</ul>"
     )
 
-
-  selectAttr();
-}
-
-function selectAttr(playerCard) {
-  $('#gameContainer').one('click', 'li', function() {
-    console.log('click');
-    $(this).css("background", "green");
-    var playerAttrVal = parseInt($(this).text().replace(/[^0-9]/g, '')); //this is ragex syntax--    
-
-    console.log(playerAttrVal);
-    
-  });
-};
-
-function evaluateTrickWinner(){
-  if (playerAttrVal === computerAttrVal){
-    alert("It's a draw for that attribute! Please choose another one.");
-  } else if (playerAttrVal > computerAttrVal) {
-    playerWinsTrick();
-  } else {
-    computerWinsTrick();
+    appendScores(player, id)
   }
-}
 
-function playerWinsTrick(){
+  function appendScores(player, id) {
+    $.each(player, function(key, value) {
+      if (typeof(value) === "number" ) {
+        $('.'+id+' figure.back ul').append(
+          "<li class=" + key + ">" + key + " " + "<span>" + value + "</span></li>"
+          )
+      }
+    })
+  }
 
-}
+  function selectAttr() {
+    $('.player').one('click', 'li', function() {
+
+      $(this).css("background", "yellow");
+
+      var attr         = $(this).attr('class');
+      var playerMove   = $(this)[0];
+      var computerMove = $('.computer figure.back ul li.' + attr)[0]
+
+      console.log(playerMove, computerMove)
+      $('.computer').addClass('flipped');
+
+      getWinner(playerMove, computerMove)        
+    });
+  };
+
+  function getWinner(player, computer) {
+    var  a = $(player).find("span")[0];
+    var  b = $(computer).find("span")[0];
+
+    var playerScore = parseInt($(a).text());
+    var computerScore = parseInt($(b).text());
+
+    console.log(playerScore, computerScore);
+
+    if ( playerScore > computerScore ) {
+      // player wins
+    } else if ( playerScore < computerScore) {
+      // computer wins
+    } else {
+      // draw
+    }
+  }
+
+  function evaluateTrickWinner(){
+    if (playerScore === computerScore){
+      alert("It's a draw for that attribute! Please choose another one.");
+    } else if (playerScore > computerScore) {
+      playerWinsTrick();
+    } else {
+      computerWinsTrick();
+    }
+  console.log(evaluateTrickWinner)
+  }
+
+  function playerWinsTrick(){
+    for (var i = 0; i < 10; i++){
+
+    }
+  }
 
 
-var playerSelectAttr = selectAttr(playerCard)       // player picks an attribute he thinks will beat the computer's score for that same attribute
-var computerSelectAttr = selectAttr(computerCard)   // returns the value on the computer's card for that same attribute
+  var playerSelectAttr = selectAttr(playerCard)       // player picks an attribute he thinks will beat the computer's score for that same attribute
+  var computerSelectAttr = selectAttr(computerCard)   // returns the value on the computer's card for that same attribute
 
-// console.log(playerSelectAttr) 
-// console.log(computerSelectAttr)
-
-
-
-// function compareScores() {
-//   if (playerAttrVal > computerAttrVal)
-
-
-  // compare both scores and evaluate a winner
-
-
-
-// $(document).ready(function(){
-//   $("li").mouseover(function(){
-//     $(this).css("cursor","pointer");
-//         $(this).animate({width: "50%", height: "50%"}, 'slow');
-//   })
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
